e76cc66ceaeadcceb04164e3110790a0f9a1f24b
Evan Mattiza <emattiza@gmail.com>
2023-05-26 15:09:30 +0000

    add some more tests

--- lib-terracrab/src/lib.rs
+++ lib-terracrab/src/lib.rs
@@ -2,7 +2,7 @@ use core::any::Any;
 use std::error::Error;
 use std::ffi::OsStr;
 use serde_json::Value;
-use std::fs::{self, read, read_to_string, write, remove_file};
+use std::fs::{self, read, read_to_string, write, remove_file, File};
 use std::path::{self, Path};
 use std::io::Result;
 use std::path::PathBuf;
@@ -13,6 +13,9 @@ struct TFStateStore {
 }
 
 impl TFStateStore {
+    // Has a caller burden to check lock before get or put
+    // Useful when client is correctly implemented
+    // Consider reimplementing if guarantees must be stronger
     pub fn new(path: &Path) -> Result<Self> {
         fs::create_dir_all(path)?;
         Ok(TFStateStore {
@@ -67,10 +70,63 @@ impl TFStateStore {
 
 #[cfg(test)]
 mod tests {
+    use std::env::temp_dir;
+
+    use tempfile::NamedTempFile;
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
+    fn it_can_write_a_file() {
+        let temp_file = NamedTempFile::new().unwrap();
+        let mut store = TFStateStore::new(temp_file.path().parent().unwrap()).unwrap();
+        let id = "xxx";
+        let put_action = store.put(id, &Value::Null);
+        assert_eq!(put_action.is_ok(), true);
+        let mut found_file: PathBuf = temp_file.path().parent().unwrap().into();
+        found_file.push(id);
+        assert_eq!(found_file.exists(), true);
+    }
+
+    #[test]
+    fn it_can_get_an_existing_file() {
+        let temp_file = NamedTempFile::new().unwrap();
+        let mut store = TFStateStore::new(temp_file.path().parent().unwrap()).unwrap();
+        let id = "xxx";
+        let put_action = store.put(id, &Value::Null);
+        assert_eq!(put_action.is_ok(), true);
+        let mut found_file: PathBuf = temp_file.path().parent().unwrap().into();
+        found_file.push(id);
+        assert_eq!(found_file.exists(), true);
+        let output = store.get(id);
+        assert_eq!(output, Some(Value::Null));
+
+    }
+
+    #[test]
+    fn it_can_lock() {
+        let temp_file = NamedTempFile::new().unwrap();
+        let mut store = TFStateStore::new(temp_file.path().parent().unwrap()).unwrap();
+        let id = "xxx";
+        let lock = store.lock(id, &Value::Null);
+        let mut found_lock: PathBuf = temp_file.path().parent().unwrap().into();
+        found_lock.push(id);
+        found_lock.set_extension("lock");
+        assert_eq!(found_lock.exists(), true);
+        assert_eq!(read_to_string(found_lock).unwrap(), "null");
+    }
+    #[test]
+    fn it_can_unlock() {
+        let temp_file = NamedTempFile::new().unwrap();
+        let mut store = TFStateStore::new(temp_file.path().parent().unwrap()).unwrap();
+        let id = "xxx";
+        let lock = store.lock(id, &Value::Null);
+        let mut found_lock: PathBuf = temp_file.path().parent().unwrap().into();
+        found_lock.push(id);
+        found_lock.set_extension("lock");
+        assert_eq!(found_lock.clone().exists(), true);
+        assert_eq!(read_to_string(found_lock.clone()).unwrap(), "null");
+        store.unlock(id, &Value::Null).unwrap();
+        assert_eq!(found_lock.exists(), false);
     }
 }
